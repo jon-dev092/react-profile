eebeea7e43ae75b15bea4bd9b511531abb50ce22
jon-dev092 <jp106833@yahoo.com>
2023-03-12 01:19:11 -0500

    added navbar componet

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+ 
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Jonathan Perez</h1>
+            <div className="links">
+                <Link to="/">About Me</Link>
+                <Link to="/project">Projects</Link>
+                <Link to="/contact">Contact Me</Link>
+                <Link to="/resume">Resume</Link>
+            </div>
+        </nav>
+     );
+}
+export default Navbar;

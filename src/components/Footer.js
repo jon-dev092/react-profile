d2451d661af01eecb4ad239b47a52e62945a1ff9
jon-dev092 <jp106833@yahoo.com>
2023-03-12 19:59:06 -0400

    added new footer with links visable on every route, styles added to index.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
+import React from "react";
+import GithubLogo from '../img/github-logo.png';
+import LinkedInLogo from '../img/Linkedin.png';
+import TwitterLogo from '../img/twitter-logo.png';
+
 const Footer = () => {
     return ( 
         <div className="footer">
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
-            <a href=""><img src="" alt="" /></a>
+            <a className="logo" href="https://github.com/jon-dev092"><img className="logo" src={GithubLogo} alt="" /></a>
+            <a className="logo" href="https://www.linkedin.com/"><img className="logo" src={LinkedInLogo} alt="" /></a>
+            <a className="logo" href="https://twitter.com/"><img className="logo" src={TwitterLogo} alt="" /></a>
         </div>
      );
 }

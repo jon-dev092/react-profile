88fe017aa04deec5da04f5e0a45fa31e9a9de55e
jon-dev092 <jp106833@yahoo.com>
2023-03-12 21:31:16 -0400

    added resume and link to full resume on resume route; updated about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -10,7 +10,9 @@ const AboutMe = () => {
                 I come from a back-ground in military marine avitaion ordinance aswell as hold a degree in welding technology. 
                 My aim is to bring my leadership skills and dilligent work ethic into the field of technology where I can create software that changes the lives of individuals all over the globe. 
                 I hold many interests that comprise from anything related to martial arts, gaming, movies/music, and ofcourse technology. 
-                I hope to continue to persue the best version of myself except also be a passionate computer programmer. Examples of my work can be found on the "Projects" tab on top!</p>
+                I hope to continue to persue the best version of myself except also be a passionate computer programmer. Dont hesitate to contact me by navigating to the "Contact Me" tab on top to send me an email;
+                further examples of my work can be found on the "Projects" tab aswell.
+            </p>
         </div>
      );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,15 @@
+import resume from '../img/resume.png'
 
 // Resume for employers to review
+// click the resume on the browser to see the full version
 const Resume = () => {
     return ( 
         <div className="resume">
             <h2>Resume</h2>
-            <p>Here is my resume</p>
+            <p>Check out my full resume by clicking on it!</p>
+            <br />
+            <br />
+            <a href="https://docs.google.com/document/d/17wFUE_J7G4UNaWl7lPgwT1DmCKKRlc-isAnrJjyq2iU/edit"><img src={resume} alt="jonathan's resume" /></a>
         </div>
      );
 }
